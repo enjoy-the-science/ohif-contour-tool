34b1089a2cbef258e4ad6d30d53c43ed9a61691e
Ekaterina <ekaterina13091997@yandex.ru>
2019-12-11 17:54:37 +0300

    check scale_image. don't work

--- cont_fill_tool/src/cont-fill-tool.js
+++ cont_fill_tool/src/cont-fill-tool.js
@@ -83,13 +83,38 @@ export default class CountourFillTool extends BaseBrushTool {
 
 
   proceedCalculations(evt){
+
     //logic of getting coordinates to brush
     //get Image
     const image = evt.detail.image;
     const imagePixelData = image.getPixelData();
     const imageWidth = image.width;
     const imageHeight = image.height; //is not grayscale
-    //TODO graycale
+    
+    //TODO
+    
+
+
+    //TODO graycale (np.maximum(image_2d, 0) / image_2d.max()) * 255.0
+    //scale image (пока int)
+/*
+    function scale_image(imageData){
+      let image_max = getMaxOfArray(imageData);
+      let image_scaled = new Int16Array();
+      for (let i = 0; i<imageData.length;i++){
+        let k = imageData[i]/image_max;
+        //console.log(k*255);//
+        image_scaled[i] = Math.round(k*255);
+      }
+      return image_scaled;
+    }
+*/
+    //console.log(imagePixelData);
+    //console.log(getMaxOfArray(imagePixelData));
+    //console.log(imagePixelData[30674]);
+    //let k = imagePixelData[30674]/getMaxOfArray(imagePixelData);
+    //console.log(k*255);
+   //const image_scale = scale_image(imagePixelData);
 
     //cut fragment for count threshold
     let xS = this.startCoords.x.valueOf();
@@ -104,26 +129,13 @@ export default class CountourFillTool extends BaseBrushTool {
     const o_thresh = otsu_threshold(imagePixelData);
     console.log(o_thresh);
 
-    ///*
-    let max_val=0;
-    let min_val = imagePixelData[0];
-    for (let i =0;i<imagePixelData.length;i++){
-      if (imagePixelData[i]>max_val){
-        max_val = imagePixelData[i];
-      }
-      if (imagePixelData[i]<min_val){
-        min_val = imagePixelData[i];
-      }
-    }
 
-    console.log(max_val);
-    console.log(min_val);
-    //*/
+   
     //TODO get fragment(or contour)
     
 
     //TODO prepare data for search
-    const preparedData = prepareDataForSearch(imagePixelData, imageWidth, imageHeight);//change
+    //const preparedData = prepareDataForSearch(imagePixelData, imageWidth, imageHeight);
 
     //search contours
     const arrayPolig = searchCont(imagePixelData, o_thresh, imageWidth, imageHeight);//try const thresh
@@ -196,6 +208,10 @@ function drawBrushPixels(
 
 
 //
+function getMaxOfArray(numArray) {
+  return Math.max.apply(null, numArray);
+}
+
 function mean_thresh(highlData) {
   let threshold = 0;
   let sum = 0;
@@ -221,6 +237,7 @@ function cutHilghFragm(xS, yS, xE, yE, imagePixelData) {
   return imagePixelData.slice(beginCut, endCut);
 }
 
+/*
 //wrong
 function prepareDataForSearch(fragmData, fragmWidth, fragmHeight) {
   const values = new Float64Array(fragmWidth * fragmHeight);
@@ -232,6 +249,49 @@ function prepareDataForSearch(fragmData, fragmWidth, fragmHeight) {
   }
   return values;
 }
+*/
+function otsu_threshold(highlData) {
+
+  let n =getMaxOfArray(highlData);
+  let histogram = Array(n);
+
+  for (let i = 0; i < n; ++i){
+    histogram[i] = 0;
+    }
+
+  for (let i = 0; i < highlData.length; i++){
+    histogram[highlData[i]] += 1;
+  }
+  
+  var sum = 0
+  , sumB = 0
+  , wB = 0
+  , wF = 0
+  , mB
+  , mF
+  , max = 0
+  , between
+  , threshold = 0;
+
+  for (let i = 0; i < 256; ++i) {
+    wB += histogram[i];
+    if (wB == 0)
+        continue;
+    wF = highlData.length - wB;
+    if (wF == 0)
+        break;
+    sumB += i * histogram[i];
+    mB = sumB / wB;
+    mF = (sum - sumB) / wF;
+    between = wB * wF * Math.pow(mB - mF, 2);
+    if (between > max) {
+        max = between;
+        threshold = i;
+    }
+}
+  return threshold;
+}
+
 
 function searchCont(preparedData, mean_thresh, fragmWidth, fragmHeight){
   let contoursArray = d3.contours().size([fragmWidth, fragmHeight]);
@@ -239,187 +299,10 @@ function searchCont(preparedData, mean_thresh, fragmWidth, fragmHeight){
   return contours.coordinates;
 }
 
+
+
 function roundPoint(coordinates){
   return [ Math.round(coordinates[0]), Math.round(coordinates[1]) ];
 }
 
-function otsu_threshold(highlData) {
-
-  let histogram = Array(256);
-
-  for (let i = 0; i < 256; ++i){
-    histogram[i] = 0;
-    }
-
-  for (let i = 0; i < highlData.length; i++){
-    histogram[highlData[i]] += 1;
-  }
-  
-  var sum = 0
-  , sumB = 0
-  , wB = 0
-  , wF = 0
-  , mB
-  , mF
-  , max = 0
-  , between
-  , threshold = 0;
-
-  for (let i = 0; i < 256; ++i) {
-    wB += histogram[i];
-    if (wB == 0)
-        continue;
-    wF = highlData.length - wB;
-    if (wF == 0)
-        break;
-    sumB += i * histogram[i];
-    mB = sumB / wB;
-    mF = (sum - sumB) / wF;
-    between = wB * wF * Math.pow(mB - mF, 2);
-    if (between > max) {
-        max = between;
-        threshold = i;
-    }
-}
-  return threshold;
-}
-
-/*
-TODO : 
-
-import csTools from "cornerstone-tools"
-import csCore from "cornerstone-core";
-import * as d3 from "d3";
-
-const BaseBrushTool = csTools.importInternal("base/BaseBrushTool");
-const external = csTools.importInternal("externalModules");
-const segmentationModule = csTools.getModule('segmentation');
-
-//Otsu threshold?
-function otsu_threshold(highlData) {
-
-  let histogram = Array(256);
-
-  for (let i = 0; i < 256; ++i){
-    histogram[i] = 0;
-    }
-
-  for (let i = 0; i < highlData.length; i++){
-    histogram[highlData[i]] += 1;
-  }
-  
-  var sum = 0
-  , sumB = 0
-  , wB = 0
-  , wF = 0
-  , mB
-  , mF
-  , max = 0
-  , between
-  , threshold = 0;
-
-  for (let i = 0; i < 256; ++i) {
-    wB += histogram[i];
-    if (wB == 0)
-        continue;
-    wF = highlData.length - wB;
-    if (wF == 0)
-        break;
-    sumB += i * histogram[i];
-    mB = sumB / wB;
-    mF = (sum - sumB) / wF;
-    between = wB * wF * Math.pow(mB - mF, 2);
-    if (between > max) {
-        max = between;
-        threshold = i;
-    }
-}
-  return threshold;
-}
-
-
-function mean_thresh(highlData) {
-    let threshold = 0;
-    let sum = 0;
-    for (let i = 0; i < highlData.length; i++) {
-        sum = sum + highlData[i];
-    }
-    threshold = sum / highlData.length;
-    return threshold; //check (1-(threshold/255)?)
-}
-
-function cutHilghFragm(xS, yS, xE, yE, imagePixelData) {
-    let xStart = Math.round(xS);
-    let yStart = Math.round(yS);
-    let xEnd = Math.round(xE);
-    let yEnd = Math.round(yE);
-    let widthHighl = Math.abs(xStart - xEnd);
-    let heightHighl = Math.abs(yStart - yEnd);
-    let distance = widthHighl * heightHighl;
-    let xCut = Math.min(xStart, xEnd);
-    let yCut = Math.min(yStart, yEnd);
-    let beginCut = xCut * yCut;
-    let endCut = beginCut + distance;
-    return imagePixelData.slice(beginCut, endCut);
-}
-
-function prepareDataForSearch(fragmData, fragmWidth, fragmHeight) {
-    const values = new Float64Array(fragmWidth * fragmHeight);
-    //StackBlur.R(imageData, 3); TODO import function
-    for (let j = 0, k = 0; j < fragmHeight; ++j) {
-        for (let i = 0; i < fragmWidth; ++i, ++k) {
-            values[k] = fragmData[(k << 2)]; //check (/255&)
-        }
-    }
-    return values;
-}
-
-function searchCont(preparedData, mean_thresh, fragmWidth, fragmHeight) {
-    let contoursArray = d3.contours().size([fragmWidth, fragmHeight]);
-    let contours = contoursArray.contour(preparedData, mean_thresh);
-    return contours.coordinates;
-
-}
-
-
-    proceedCalculations(evt) {
-        console.log("calculations. Dots start:" + JSON.stringify(this.startCoords) + "Dots finish: " + JSON.stringify(this.finishCoords));
-
-        //logic of getting coordinates to brush
-
-        //get Image (clean image?)
-        const image = evt.detail.image;
-        const imagePixelData = image.getPixelData();
-        const imageWidth = image.width;
-        const imageHeight = image.height;
-
-        //grayscale?
-
-        //cut fragment for count threshold
-        let xS = this.startCoords.x.valueOf();
-        let yS = this.startCoords.y.valueOf();
-        let xE = this.finishCoords.x.valueOf();
-        let yE = this.finishCoords.y.valueOf();
-        const highlFragment = cutHilghFragm(xS, yS, xE, yE, imagePixelData);
-        console.log(highlFragment);//
-
-        //count threshold (invert?)
-        const mean_threshold = mean_thresh(highlFragment);
-        console.log(mean_threshold);
-
-
-        //TODO get fragment?
-
-        //prepare data for search ?
-        const preparedData = prepareDataForSearch(imagePixelData, imageWidth, imageHeight);
-
-        //search contours
-        const arrayPolig = searchCont(preparedData, mean_threshold, imageWidth, imageHeight);
-        console.log(arrayPolig);//
-
-        this.draw(evt);
-    }
-
-
-*/
 
