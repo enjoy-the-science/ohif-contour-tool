d6bf8b55a37eeb446a8d100998bcd2c31e73f6dd
Ekaterina <ekaterina13091997@yandex.ru>
2020-01-01 15:20:54 +0300

    add renderBrush method

--- cont_fill_tool_v2/src/cont-fill-tool.js
+++ cont_fill_tool_v2/src/cont-fill-tool.js
@@ -13,7 +13,7 @@ export default class CountourFillTool extends BaseBrushTool {
             name: 'CountourFill',
             supportedInteractionTypes: ['Mouse', 'Touch'],
             configuration: {},
-            mixins: ['renderBrushMixin'],
+            //mixins: ['renderBrushMixin'],
         };
 
         super(props, defaultProps);
@@ -147,7 +147,69 @@ export default class CountourFillTool extends BaseBrushTool {
         cornerstone.updateImage(evt.detail.element);
     }
 
-    //TODO renderBrush cursor
+    //TODO cursor
+    renderBrush(evt) {
+        const {getters, configuration} = segmentationModule;
+        const eventData = evt.detail;
+        const viewport = eventData.viewport;
+
+        let mousePosition;
+
+        if (this._drawing) {
+            mousePosition = this._lastImageCoords;
+        } else if (this._mouseUpRender) {
+            mousePosition = this._lastImageCoords;
+            this._mouseUpRender = false;
+        } else {
+            mousePosition = csTools.store.state.mousePositionImage;
+        }
+
+        if (!mousePosition) {
+            return;
+        }
+
+        const {rows, columns} = eventData.image;
+        const {x, y} = mousePosition;
+
+        if (x < 0 || x > columns || y < 0 || y > rows) {
+            return;
+        }
+
+        const radius = configuration.radius;
+        const context = eventData.canvasContext;
+        const element = eventData.element;
+        const color = getters.brushColor(element, this._drawing);
+
+        context.setTransform(1, 0, 0, 1, 0, 0);
+
+        let circleRadius = radius * viewport.scale;
+        const mouseCoordsCanvas = window.cornerstone.pixelToCanvas(
+            element,
+            mousePosition,
+        );
+
+        const {labelmap2D} = getters.labelmap2D(element);
+
+        const getPixelIndex = (x, y) => y * columns + x;
+        const spIndex = getPixelIndex(Math.floor(x), Math.floor(y));
+        const isInside = labelmap2D.pixelData[spIndex] === 1;
+        this.shouldErase = !isInside;
+        context.beginPath();
+        context.strokeStyle = color;
+        context.ellipse(
+            mouseCoordsCanvas.x,
+            mouseCoordsCanvas.y,
+            circleRadius,
+            circleRadius,
+            0,
+            0,
+            2 * Math.PI,
+        );
+        context.stroke();
+
+        this._lastImageCoords = eventData.image;
+    }
+
     //TODO fix bag
     //TODO clean and document code
 }
@@ -208,9 +270,10 @@ function get_max_diff(data) {
         }
     }
 
-    return max_diff;
+    return max_diff + max_diff * 0.05; //?
 }
 
+//work with tolerance
 function count_tolerance(deltaX, deltaY, coeff) {
 
     if (deltaY === 0) {
