2f718edd09c1720b2fc50963b8a3d73197ee4149
Ekaterina <ekaterina13091997@yandex.ru>
2020-01-04 11:43:24 +0300

    work with cursor

--- cont_fill_tool_v2/src/cont-fill-tool.js
+++ cont_fill_tool_v2/src/cont-fill-tool.js
@@ -7,8 +7,6 @@ import floodFill from "n-dimensional-flood-fill";
 const {drawBrushPixels} = csTools.importInternal(
     'util/segmentationUtils'
 );
-
-
 const segmentationModule = csTools.getModule('segmentation');
 
 export default class CountourFillTool extends BaseBrushTool {
@@ -21,33 +19,55 @@ export default class CountourFillTool extends BaseBrushTool {
 
         super(props, defaultProps);
 
-        this.touchDragCallback = this._paint.bind(this);
         this.preMouseDownCallback = this.preMouseDownCallback.bind(this);
-        this.mouseDragCallback = this._paint.bind(this);
-
+        this._drawingMouseUpCallback = this._drawingMouseUpCallback.bind(this);
+        this.init = this.init.bind(this);
+        this.renderBrush = this.renderBrush.bind(this);
+        this.mouseDragCallback = this.mouseDragCallback.bind(this);
+        this._paint = this._paint.bind(this);
     }
 
+    init(evt) {
+        
+        const eventData = evt.detail;
+        const element = eventData.element;
+
+        this.rows = eventData.image.rows;
+        this.columns = eventData.image.columns;
+
+        const {getters} = segmentationModule;
+
+        const {
+            labelmap2D,
+            labelmap3D,
+            currentImageIdIndex,
+            activeLabelmapIndex,
+        } = getters.labelmap2D(element);
+
+        const shouldErase =
+            super._isCtrlDown(eventData) || this.configuration.alwaysEraseOnClick;
+
+        this.paintEventData = {
+            labelmap2D,
+            labelmap3D,
+            currentImageIdIndex,
+            activeLabelmapIndex,
+            shouldErase,
+        };
+    }
 
     preMouseDownCallback(evt) {
 
         const eventData = evt.detail;
+        this.init(evt);
         const {element, currentPoints} = eventData;
-        const {rows, columns} = eventData.image;
-        this._startPainting(evt);
-
-        //get start coordinate
         this.startCoords = currentPoints.image;
 
-        //get labelmap
-        const {getters} = segmentationModule;
-        const {labelmap2D} = getters.labelmap2D(eventData.element);
-        const PixelData2D = get_2DArray(labelmap2D.pixelData, rows, columns);
+        const PixelData2D = get_2DArray(this.paintEventData.labelmap2D.pixelData, this.rows, this.columns);
 
-        //get imagePixelData
         const imagePixelData = eventData.image.getPixelData();
-        this.imagePixelData2D = get_2DArray(imagePixelData, rows, columns);
+        this.imagePixelData2D = get_2DArray(imagePixelData, this.rows, this.columns);
 
-        //get contour area
         let result = floodFill({
             getter: function (x, y) {
                 return PixelData2D[y][x];
@@ -61,7 +81,6 @@ export default class CountourFillTool extends BaseBrushTool {
 
         this.area = result.flooded;
 
-        //get coeff for count tolerance
         let areaPixelData = [];
         for (let i = 0; i < this.area.length; i++) {
             areaPixelData.push(this.imagePixelData2D[this.area[i][0]][this.area[i][1]]);
@@ -70,22 +89,44 @@ export default class CountourFillTool extends BaseBrushTool {
 
         this._drawing = true;
         super._startListeningForMouseUp(element);
-        this._paint(evt);
+        this._lastImageCoords = currentPoints.image;
 
         return true;
+    }
 
+    mouseDragCallback(evt) {
+
+        const {currentPoints} = evt.detail;
+
+        this.finishCoords = currentPoints.image;
+        this._lastImageCoords = currentPoints.image;
+
+        this.renderBrush(evt);
+        this._paint(evt);
+    }
+
+    _drawingMouseUpCallback(evt) {
+        const eventData = evt.detail;
+        const {element, currentPoints} = eventData;
+
+        this.finishCoords = currentPoints.image;
+
+        this._drawing = false;
+        super._stopListeningForMouseUp(element);
     }
 
     _paint(evt) {
 
-        const eventData = evt.detail;
-        const {rows, columns} = eventData.image;
+
+        const rows = this.rows;
+        const columns = this.columns;
 
         let xS = this.startCoords.x.valueOf();
         let yS = this.startCoords.y.valueOf();
 
-        //get current points
-        const {x, y} = eventData.currentPoints.image;
+        let x = this.finishCoords.x.valueOf();
+        let y = this.finishCoords.y.valueOf();
+
 
         if (x < 0 || x > columns || y < 0 || y > rows) {
             return;
@@ -105,19 +146,17 @@ export default class CountourFillTool extends BaseBrushTool {
             return false;
         };
 
-        //count tolerance
-        const coeff = this.max_diff;
+        const k = this.max_diff;
 
         const get_delta = (pointSt, pointFin) => {
             return Math.abs(pointSt - pointFin)
         };
 
-        const tolerance = count_tolerance(get_delta(xS, x), get_delta(yS, y), coeff);
+        const tolerance = count_tolerance(get_delta(xS, x), get_delta(yS, y), k);
         console.log(`tolerance ${tolerance}`);
 
         const {labelmap2D, labelmap3D} = this.paintEventData;
 
-        //Flood fill
         let result = floodFill({
             getter: function (x, y) {
                 if (inArea(x, y)) {
@@ -134,7 +173,7 @@ export default class CountourFillTool extends BaseBrushTool {
 
         let pointerArray = result.flooded;
 
-        //? добавить сглаживание контура + заполнить пробелы
+        //?добавить сглаживание контура + заполнить пробелы
         drawBrushPixels(
             pointerArray,
             labelmap2D.pixelData,
@@ -147,75 +186,117 @@ export default class CountourFillTool extends BaseBrushTool {
     }
 
 
-    //cursor
     renderBrush(evt) {
-        const {getters, configuration} = segmentationModule;
+        const {getters} = segmentationModule;
         const eventData = evt.detail;
         const viewport = eventData.viewport;
 
         let mousePosition;
 
+        if (this._drawing) {
+            mousePosition = this._lastImageCoords; //end ellipse point
+            let startPointForDrawing = this.startCoords; //start ellipse point
 
-        /*
+            if (!mousePosition) {
+                return;
+            }
 
-        */
+            const {rows, columns} = eventData.image;
+            const {x, y} = mousePosition;
 
-        //if (this._drawing) {
-        //    mousePosition = this._lastImageCoords;
-        //} else
-        if (this._mouseUpRender) {
-            mousePosition = this._lastImageCoords;
-            this._mouseUpRender = false;
+            if (x < 0 || x > columns || y < 0 || y > rows) {
+                return;
+            }
+
+            const radius = 1;
+            const context = this.context;
+            const element = eventData.element;
+            const color = getters.brushColor(element, this._drawing);
+
+            context.setTransform(1, 0, 0, 1, 0, 0);
+
+            const {labelmap2D} = getters.labelmap2D(element);
+
+            const getPixelIndex = (x, y) => y * columns + x;
+            const spIndex = getPixelIndex(Math.floor(x), Math.floor(y));
+            const isInside = labelmap2D.pixelData[spIndex] === 1;
+            this.shouldErase = !isInside;
+            context.beginPath();
+            context.strokeStyle = color;
+            context.fillStyle = "rgba(128,128,128,0.5)";
+
+            const startCoordsCanvas = window.cornerstone.pixelToCanvas(
+                element,
+                startPointForDrawing,
+            );
+
+            let width = Math.abs(startPointForDrawing.x - mousePosition.x) * viewport.scale;
+            let height = Math.abs(startPointForDrawing.y - mousePosition.y) * viewport.scale;
+
+            context.ellipse(
+                startCoordsCanvas.x,
+                startCoordsCanvas.y,
+                width,
+                height,
+                0,
+                0,
+                2 * Math.PI,
+            );
+            context.stroke();
+            context.fill();
+
+            this._lastImageCoords = eventData.image;
         } else {
             mousePosition = csTools.store.state.mousePositionImage;
+
+            if (!mousePosition) {
+                return;
+            }
+
+            const {rows, columns} = eventData.image;
+            const {x, y} = mousePosition;
+
+            if (x < 0 || x > columns || y < 0 || y > rows) {
+                return;
+            }
+
+            const radius = 1;
+            const context = eventData.canvasContext;
+            this.context = eventData.canvasContext;
+            const element = eventData.element;
+            const color = getters.brushColor(element, this._drawing);
+
+            context.setTransform(1, 0, 0, 1, 0, 0);
+
+            let circleRadius = radius * viewport.scale;
+            const mouseCoordsCanvas = window.cornerstone.pixelToCanvas(
+                element,
+                mousePosition,
+            );
+
+            const {labelmap2D} = getters.labelmap2D(element);
+
+            const getPixelIndex = (x, y) => y * columns + x;
+            const spIndex = getPixelIndex(Math.floor(x), Math.floor(y));
+            const isInside = labelmap2D.pixelData[spIndex] === 1;
+            this.shouldErase = !isInside;
+            context.beginPath();
+            context.strokeStyle = color;
+            context.fillStyle = "rgba(128,128,128,0.5)";
+            context.ellipse(
+                mouseCoordsCanvas.x,
+                mouseCoordsCanvas.y,
+                circleRadius,
+                circleRadius,
+                0,
+                0,
+                2 * Math.PI,
+            );
+            context.stroke();
+            context.fill();
+
+            this._lastImageCoords = eventData.image;
         }
-
-        if (!mousePosition) {
-            return;
-        }
-
-        const {rows, columns} = eventData.image;
-        const {x, y} = mousePosition;
-
-        if (x < 0 || x > columns || y < 0 || y > rows) {
-            return;
-        }
-
-        const radius = configuration.radius;
-        const context = eventData.canvasContext;
-        const element = eventData.element;
-        const color = getters.brushColor(element, this._drawing);
-
-        context.setTransform(1, 0, 0, 1, 0, 0);
-
-        let circleRadius = radius * viewport.scale;
-        const mouseCoordsCanvas = window.cornerstone.pixelToCanvas(
-            element,
-            mousePosition,
-        );
-
-        const {labelmap2D} = getters.labelmap2D(element);
-
-        const getPixelIndex = (x, y) => y * columns + x;
-        const spIndex = getPixelIndex(Math.floor(x), Math.floor(y));
-        const isInside = labelmap2D.pixelData[spIndex] === 1;
-        this.shouldErase = !isInside;
-        context.beginPath();
-        context.strokeStyle = color;
-        context.fillStyle = "rgba(128,128,128,0.5)";
-        context.ellipse(
-            mouseCoordsCanvas.x,
-            mouseCoordsCanvas.y,
-            circleRadius,
-            circleRadius,
-            0,
-            0,
-            2 * Math.PI,
-        );
-        context.stroke();
-        context.fill();
-
-        this._lastImageCoords = eventData.image;
     }
 
 }
@@ -237,10 +318,10 @@ function get_max_diff(data) {
 
     let max_diff = 1;
     for (let i = 0; i < data.length; i++) {
-        let buf = data[i];
+        let buff = data[i];
         for (let j = 0; j < data.length; j++) {
-            if (Math.abs(buf - data[j]) > max_diff) {
-                max_diff = Math.abs(buf - data[j]);
+            if (Math.abs(buff - data[j]) > max_diff) {
+                max_diff = Math.abs(buff - data[j]);
             }
         }
     }
@@ -249,19 +330,17 @@ function get_max_diff(data) {
 }
 
 //?(максимальная граница + более медленный рост + гибкое изменение)
-function count_tolerance(deltaX, deltaY, coeff) {
+function count_tolerance(deltaX, deltaY, k) {
 
     if (deltaY === 0) {
-        return coeff * Math.tanh(0.02 * deltaX);
+        return k * Math.tanh(0.02 * deltaX);
     } else if (deltaX === 0) {
-        return coeff * Math.tanh(0.02 * deltaY);
+        return k * Math.tanh(0.02 * deltaY);
     } else {
-        return coeff * Math.tanh(0.02 * (deltaY + deltaX));
+        return k * Math.tanh(0.02 * (deltaY + deltaX));
     }
 }
 
 
-//TODO cursor
-//TODO result flood fill, (tolerance)
-//TODO clean(+refactor) and document code + fix bag
+
 
